b5fc9de9dcb085147ae74fc17d950e5eb2f13ce4
Rafael <144218473+RafaelFFW@users.noreply.github.com>
2025-08-14 16:51:20 +0200

    [nvim] Add note-comment I found in another init.lua

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -1,2 +1,12 @@
 require("settings.opts")
 require("settings.keymap")
+
+-- NOTE: You can require modules in a non-failing way, eg. if the code is distributed and not every distribution has a local-settings file
+-- pcall(require, "local-keymap")
+-- vim.cmd(":color unokai") -- This command will still run
+-- NOTE: you'd handle error state like this
+-- local status, lfs = pcall(require, "lfs")
+-- if(status) then
+--    --lfs exists, so use it.
+-- end
+-- More information: https://www.lua.org/pil/8.4.html
