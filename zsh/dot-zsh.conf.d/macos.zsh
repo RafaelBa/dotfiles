47a421ae05c5cf0451d2e21ccf98c540ddd292b2
Rafael <144218473+RafaelFFW@users.noreply.github.com>
2026-03-05 19:00:55 +0100

    [zsh] Fix brew init, add jobs 'fix' for mac, add grep alias for color by default

--- zsh/dot-zsh.conf.d/macos.zsh
+++ zsh/dot-zsh.conf.d/macos.zsh
@@ -2,3 +2,8 @@
 # MacOS keybindings
 bindkey "^[[1;3D" backward-word # ALT-left-arrow  ⌥ + ←
 bindkey "^[[1;3C" forward-word  # ALT-right-arrow ⌥ + →
+
+# MacOS (v26 on 2026-03-04) prints the same information on -p and -l, but we expect only the PID on -p
+function jobs-p () {
+    jobs -l $1 | awk '{print $3}'
+}
--- zsh/dot-zsh.conf.d/zprofile/10-brew.zsh
+++ zsh/dot-zsh.conf.d/zprofile/10-brew.zsh
@@ -10,3 +10,7 @@ if [[ ! "$PATH" =~ (^|:)"${HOMEBREW_BIN_PATH}"(:|$) ]]
 then
     PATH=${HOMEBREW_BIN_PATH}:$PATH
 fi
+
+# `shellenv` installs brew's auto-completions for zsh (technically it adds the path with the auto-completions to $fpath / $FPATH)
+# `shellenv` MUST be called before compinit
+eval "$(brew shellenv)"
