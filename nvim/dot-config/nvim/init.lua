424dc2398e3e4a9de0036836cc1cc6f076d3b972
Rafael <11751298+RafaelBa@users.noreply.github.com>
2026-03-13 16:11:11 +0100

    [nvim] Add user_ide global var to init; deactivate line-number switching for inactive windows for now

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -2,6 +2,19 @@ require("settings.opts")
 require("settings.keymap")
 require("autocommands.line-numbers")
 
+local function isEmpty(s)
+	return s == nil or s == ''
+end
+
+if not isEmpty(vim.g.user_ide) then
+	local initFile = "init-" .. vim.g.user_ide
+	local status, error = pcall(require, initFile)
+	if not status then
+		print("WARNING: Could not load " .. initFile)
+		print("error: " .. error)
+	end
+end
+
 -- NOTE: You can require modules in a non-failing way, eg. if the code is distributed and not every distribution has a local-settings file
 -- pcall(require, "local-keymap")
 -- vim.cmd(":color unokai") -- This command will still run
--- nvim/dot-config/nvim/lua/autocommands/line-numbers.lua
+++ nvim/dot-config/nvim/lua/autocommands/line-numbers.lua
@@ -10,32 +10,34 @@ local function set_absolute_numbers()
   vim.opt.number = true
 end
 
+-- TODO: the behaviour is janky in windows like neotree or splashscreen, that by default don't have line numbers
+
 -- Autocommands to switch line number styles
 -- :autocmd adds to the list of autocommands regardless of whether they are already present.  When your .vimrc file is sourced twice, the autocommands will appear twice.  To avoid this, define your autocommands in a group, so that you can easily clear them:
-vim.api.nvim_create_augroup("HybridLineNumbers", { clear = true })
+-- vim.api.nvim_create_augroup("HybridLineNumbers", { clear = true })
 -- Alternative to removing all autocommands - instead use a variable to ensure that Vim includes the autocommands only once:
 -- :if !exists("autocommands_loaded")
 -- :  let autocommands_loaded = 1
 -- :  au ...
 -- :endif
-vim.api.nvim_create_autocmd("FocusGained", {
-  group = "HybridLineNumbers",
-  pattern = "*", -- Pattern of file name; autocommand groups can be assigned to specific file types
-  callback = set_hybrid_numbers,
-})
--- vim.api.nvim_create_autocmd("BufEnter", {
-vim.api.nvim_create_autocmd("WinEnter", {
-  group = "HybridLineNumbers",
-  pattern = "*",
-  callback = set_hybrid_numbers,
-})
-vim.api.nvim_create_autocmd("FocusLost", {
-  group = "HybridLineNumbers",
-  pattern = "*",
-  callback = set_absolute_numbers,
-})
-vim.api.nvim_create_autocmd("WinLeave", {
-  group = "HybridLineNumbers",
-  pattern = "*",
-  callback = set_absolute_numbers,
-})
+-- vim.api.nvim_create_autocmd("FocusGained", {
+--   group = "HybridLineNumbers",
+--   pattern = "*", -- Pattern of file name; autocommand groups can be assigned to specific file types
+--   callback = set_hybrid_numbers,
+-- })
+-- -- vim.api.nvim_create_autocmd("BufEnter", {
+-- vim.api.nvim_create_autocmd("WinEnter", {
+--   group = "HybridLineNumbers",
+--   pattern = "*",
+--   callback = set_hybrid_numbers,
+-- })
+-- vim.api.nvim_create_autocmd("FocusLost", {
+--   group = "HybridLineNumbers",
+--   pattern = "*",
+--   callback = set_absolute_numbers,
+-- })
+-- vim.api.nvim_create_autocmd("WinLeave", {
+--   group = "HybridLineNumbers",
+--   pattern = "*",
+--   callback = set_absolute_numbers,
+-- })
